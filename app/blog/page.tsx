fc1bbaf173f2a18393e957d04bbcce3f2ca6b11b
Semih Şahinoğlu <semihshngl@gmail.com>
2025-08-16 14:46:17 +0300

    16.08.2025 dersi calisildi

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { Suspense } from "react";
 
-const BlogPage = () => {
-  return <div>Hello From Blog</div>;
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+type PostType = Post[];
+
+const Posts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data: PostType = await res.json();
+
+  return (
+    <ul>
+      {data.map((val) => (
+        <li key={val.id}>
+          <h1>{val.title}</h1>
+          <p>{val.body}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const BlogPage = async () => {
+  return (
+    <div>
+      Hello From Blog
+      <Suspense fallback={<p>Yükleniyor...</p>}>
+        <Posts />
+      </Suspense>
+    </div>
+  );
 };
 
 export default BlogPage;
